96105a54b7b011d4536311a09853d01bce6778e0
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2022-04-05 13:16:31 +0300

    Initial Commit

---
+++ SwiftUI-NewNavigation/SwiftUI_NewNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NewNavigationApp.swift
+//  SwiftUI-NewNavigation
+//
+//  Created by Alexander Sobolev on 05.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NewNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
